5ce6e732e0215b757b80ceedc78a95f03641f4f3
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-06-15 14:07:26 -0400

    first exercise

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 var express = require("express");
 var app = express();
 
-// "/" => "Hi there!"
 app.get("/", function(req, res){
-    res.send("Hi there!");
+    res.send("Hi there, welcome to my assignment!");
 });
 
-// "/bye" => "Goodbye!" (es6 syntax)
-app.get("/bye", (req, res) => res.send("Goodbye!"));
 
-// "/dog" => "MEOW!"
-app.get("/dog", function(req, res){
-    res.send("MEOW!");
+app.get("/speak/:animal", function(req, res){
+    var animal = req.params.animal
+    if (animal == "pig"){
+        res.send("The pig says 'OINK'");
+    } else if (animal == "cow"){
+        res.send("The cow says 'Moo'")
+    } else if (animal == "dog") {
+        res.send("The dog says 'Woof Woof!'")
+    }
+});
+
+app.get("/repeat/:word/:multiplier", function(req, res){
+    var word = req.params.word + " ";
+    var multiplier = req.params.multiplier
+    res.send(word.repeat(multiplier));
+});
+
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+    res.send("WELCOME TO THE COMMENTS PAGE!");
 });
 
 app.get("*", (req, res) => res.send("YOU ARE A STAR!"));
